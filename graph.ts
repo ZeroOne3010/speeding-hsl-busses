dfaad940344afe584a642431335f7c408b73709b
Ville Saalo <ville.saalo@iki.fi>
2022-09-30 23:09:21 +0300

    Refactor: extract function

--- graph.ts
+++ graph.ts
@@ -15,16 +15,16 @@ const chartCallback: ChartCallback = (ChartJS) => {
 };
 const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height, chartCallback });
 
+const dateToHhMmSs = (date: Date): string => {
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  return `${hours}:${minutes}:${seconds}`;
+};
+
 export const createPngChart = async (vehicleData: VehicleData): Promise<Buffer> => {
   const observations: Observation[] = vehicleData.observations;
-  var labels = observations.map((observation) => {
-    const date = new Date(observation.timestamp * 1000);
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-    const seconds = date.getSeconds().toString().padStart(2, "0");
-    const time = `${hours}:${minutes}:${seconds}`;
-    return time;
-  });
+  var labels = observations.map((observation) => dateToHhMmSs(new Date(observation.timestamp * 1000)));
 
   const values = observations.map((observation) => observation.speed);
   const borderColors = observations.map((observation) =>
